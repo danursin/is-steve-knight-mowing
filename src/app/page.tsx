d5c24ce5a1cce4a6e2624eac0073b05ae0001271
danursin <danursin@gmail.com>
2023-10-02 21:27:55 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div>I am the main div</div>
+        </main>
+    );
+}
