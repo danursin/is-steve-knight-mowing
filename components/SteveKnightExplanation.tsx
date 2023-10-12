eafeb567566dae8c22f895a2fd66367a0a30cd55
danursin <danursin@gmail.com>
2023-10-12 16:35:01 -0500

    fix: steve knight explanation

--- components/SteveKnightExplanation.tsx
+++ components/SteveKnightExplanation.tsx
@@ -5,10 +5,7 @@ const SteveKnightExplanation: React.FC = () => {
     return (
         <Segment>
             <p>Steve Knight seems to mow his grass a lot. Like a lot a lot. </p>
-            <p>
-                Sometimes it appears that he mows multiple separate times in a single day and very frequently several times in the same
-                week.
-            </p>
+            <p>Sometimes he mows multiple separate times in a single day and very frequently several times in the same week.</p>
             <p>
                 This application helps to answer the question of when he last mowed and whether Steve Knight is currently mowing the grass.
                 He likely is.
